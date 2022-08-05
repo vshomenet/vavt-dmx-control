7db804c01d715a3e3d0d0fe7f39ba483c5f9985a
Sergey Semenov <sergey@vshome.net>
2022-08-05 23:01:33 +0300

    Added. Upgrade

--- frontend/update.sh
+++ frontend/update.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+sleep 5
 cd /opt/dmx/conf
 rm sys.conf
 /usr/bin/tar -cvf /tmp/dmx_backup.tar *
 cd /opt
-/bin/systemctl stop nginx
-/bin/systemctl stop dmx
 rm -rf /opt/dmx
 mkdir -p /opt/dmx
 cp -r /tmp/dmx/frontend/* /opt/dmx
 cd /opt/dmx/conf
 /usr/bin/tar -xvf /tmp/dmx_backup.tar
 rm /tmp/dmx_backup.tar
-/bin/systemctl start dmx
-/bin/systemctl start nginx
-rm -rf /tmp/dmx
+/bin/systemctl restart dmx
