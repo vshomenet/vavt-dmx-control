40c01311d326943c9cd9c5638d78329c350a6109
Sergey Semenov <sergey@vshome.net>
2022-11-17 17:09:30 +0300

    Added. Page create and restore backup

--- frontend/classConfig.py
+++ frontend/classConfig.py
@@ -16,14 +16,35 @@ from wtforms.validators import DataRequired
 
 class GlobalVar(object):
 	def __init__(self):
-		#self.path = '/media/psf/Home/GIT/vavt-dmx/frontend'
-		self.path = '/opt/dmx'
+		self.path = '/media/psf/Home/GIT/vavt-dmx/frontend'
+		#self.path = '/opt/dmx'
 		
 	# Копирование файла sys.conf в оперативную память
 	def create_conf(self):
 		if not os.path.isfile('/dev/shm/sys.conf'):
 			os.system('cp '+self.path+'/conf/sys.conf /dev/shm/')
-        
+			
+	# Backup создание и восстановление конфигурации
+	def backup(self, param, file_name):
+		if param == 'create':
+			os.system('rm -r ' + self.path + '/download/* > /dev/null 2>&1')
+			i = os.system('cp -r ' + self.path + '/conf ' + self.path + '/download > /dev/null 2>&1')
+			i += os.system('rm ' + self.path + '/download/conf/sys.conf > /dev/null 2>&1')
+			i += os.system('tar -cjf '+ self.path + '/download/' + file_name + ' -C ' + self.path + '/download/ conf > /dev/null 2>&1')
+			i += os.system('rm -r ' + self.path + '/download/conf > /dev/null 2>&1')
+			if i == 0:
+				return True
+			return False
+		if param == 'restore':
+			i = os.system('tar -xjf '+ self.path + '/download/' + file_name + ' -C ' + self.path + '/download > /dev/null 2>&1')
+			if i == 0:
+				i += os.system('cp '+ self.path + '/download/conf/* ' + self.path + '/conf/ > /dev/null 2>&1')
+				os.system('rm -r ' + self.path + '/download/* > /dev/null 2>&1')
+				if i == 0:
+					return True
+			os.system('rm -r ' + self.path + '/download/* > /dev/null 2>&1')
+			return False
+
 class ConfigHost(object):
 	def __init__(self, path):
 		self.pathDevice = str(path)+'/conf/device.conf'
@@ -32,7 +53,7 @@ class ConfigHost(object):
 		self.pathSys = '/dev/shm/sys.conf'
 		self.main_menu = {"index":"Пресеты", "control":"Ручное управление", "login":"Вход"}
 		self.admin_menu = {"index":"Пресеты", "control":"Ручное правление", "config":"Настройки DMX", "cfg_device":"Устройства DMX", \
-						   "telegram":"Telegram", "update":"Обслуживание", "change_admin":"Администратор", "logout":"Выход"}
+						   "telegram":"Telegram", "update":"Обслуживание", "backup":"Резервная копия", "change_admin":"Администратор"}
 		self.foot = ['© Сергей Семенов sergey@vshome.net']
 
 	# Пароль и пользователь
